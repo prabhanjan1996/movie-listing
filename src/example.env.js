298dbc403aec0cc6ca019cb0fa1794f3ed3b3845
prabhanjan1996 <42670179+prabhanjan1996@users.noreply.github.com>
2021-10-20 12:04:41 +0530

    .env file sample code snippet

---
+++ src/example.env.js
@@ -0,0 +1,13 @@
+APP_NAME=Movie-listing
+NODE_ENV=development
+HTTPS=false
+HTTPS_PORT=80
+HTTP_PORT=8000
+JWT_SECRET = "jwt_secret_key"
+
+MONGO_USERNAME= Your_Cluster_Username
+MONGO_PASSWORD= Your_Cluster_Username
+MONGO_DATABASE= movies
+MONGO_URI= Your_Cluster_URI  // cluster0.s1rsk.mongodb.net
+
+UPLOAD_DIR=path/to/upload/related/to/src
